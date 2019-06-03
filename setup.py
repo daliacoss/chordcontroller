7ab23e43e74483568cfcd3c203b9469574111dd8
Decky Coss <deckycoss@gmail.com>
2019-06-03 17:18:48 -0600

    update requirements and setup.py package list

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     python_requires="~=3.3",
     install_requires=[
         "appdirs>=1.4.3",
+        "immutables>=0.9",
         # until they get rid of the annoying startup message, don't use later
         # version of pygame than 1.9.3
         "pygame==1.9.3",
